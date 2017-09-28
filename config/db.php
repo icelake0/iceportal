770856492ec67f283bae3d4c38f01d3f18512142
icelake0 <ajiboyeag@gmail.com>
2017-09-28 15:56:41 +0100

    db update

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=sql308.website.tk;dbname=wetk_20758696_test',
-    'username' => 'wetk_20758696',
-    'password' => '1swordfish',
+    'dsn' => 'mysql:host=sql308.website.tk;dbname=************',
+    'username' => '*************',
+    'password' => '************',
     'charset' => 'utf8',
 ];
